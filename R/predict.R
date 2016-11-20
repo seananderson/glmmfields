95e3bcd2a040b61681b42be810452517c56f3d15
eric-ward <eric.ward@noaa.gov>
2016-11-20 14:43:02 -0800

    update predict function

--- R/predict.R
+++ R/predict.R
@@ -1,6 +1,6 @@
 #' @export
 
-predict_rrfield <- function(fitted_model, new_data, mcmc_draws, time="time", lon="lon", lat="lat") {
+predict_rrfield <- function(fitted_model, new_data, mcmc_draws, time="time") {
 
   # newdata is df with time, y, lon, lat
 
@@ -11,11 +11,11 @@ predict_rrfield <- function(fitted_model, new_data, mcmc_draws, time="time", lon
   distKnotsSq = distKnots^2 # squared distances
 
   # Calculate distance from knots to grid
-  dist_all = as.matrix(stats::dist(rbind(new_data[, c(lon, lat)], knots)))^2
+  dist_all = as.matrix(stats::dist(rbind(new_data[, c(fitted_model$lon, fitted_model$lat)], knots)))
   n_locs = nrow(new_data)
 
   # this is the transpose of the lower left corner
-  dist_knots21_sq <- t(
+  dist_knots21 <- t(
     dist_all[-c(seq_len(n_locs)), -c((n_locs + 1):ncol(dist_all))])
 
 
@@ -26,12 +26,12 @@ predict_rrfield <- function(fitted_model, new_data, mcmc_draws, time="time", lon
   pred_values = matrix(NA, n_locs, mcmc_draws)
   for(i in 1:mcmc_draws) {
     # create cov matrix @ knots
-    if(fitted_model$gauss_cor==0) {
-      covmat = pars$gp_sigma[mcmc.i[i]] * exp(-pars$gp_scale[mcmc.i[i]]*distKnots)
-      covmat21 = pars$gp_sigma[mcmc.i[i]] * exp(-pars$gp_scale[mcmc.i[i]]*dist_knots21_sq)
+    if(fitted_model$correlation=="exponential") {
+      covmat = pars$gp_sigma[mcmc.i[i]] * exp(-distKnots/pars$gp_scale[mcmc.i[i]])
+      covmat21 = pars$gp_sigma[mcmc.i[i]] * exp(-dist_knots21/pars$gp_scale[mcmc.i[i]])
     } else {
-      covmat = pars$gp_sigma[mcmc.i[i]] * exp(-pars$gp_scale[mcmc.i[i]]*(distKnots^2))
-      covmat21 = pars$gp_sigma[mcmc.i[i]] * exp(-pars$gp_scale[mcmc.i[i]]*(dist_knots21_sq^2))
+      covmat = pars$gp_sigma[mcmc.i[i]] * exp(-2*(distKnots^2)/(pars$gp_scale[mcmc.i[i]]^2))
+      covmat21 = pars$gp_sigma[mcmc.i[i]] * exp(-2*(distKnots21^2)/(pars$gp_scale[mcmc.i[i]]^2))
     }
 
     # these are projected spatial effects, dim = new data points x time
@@ -39,7 +39,7 @@ predict_rrfield <- function(fitted_model, new_data, mcmc_draws, time="time", lon
 
     rows = seq_len(n_locs)
     cols = new_data$time
-    pred_values[,i] = pars$B[i] + spat_effects[cbind(rows,cols)] # check this for > 1 year. B will also have to be modified
+    pred_values[,i] = pars$B[mcmc.i[i],1] + spat_effects[cbind(rows,cols)] # check this for > 1 year. B will also have to be modified
   }
 
   pred_values
