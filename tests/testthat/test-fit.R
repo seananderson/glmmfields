1aa0b1a1119f0eaabe45a489da098b8b24a06a10
Sean Anderson <sean@seananderson.ca>
2016-11-22 14:52:48 -0800

    Update tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,4 +1,4 @@
-# if (interactive()) options(mc.cores = parallel::detectCores())
+if (interactive()) options(mc.cores = parallel::detectCores())
 
 ITER <- 400
 CHAINS <- 2
@@ -6,15 +6,18 @@ SEED <- 123
 
 gp_sigma <- 0.2
 sigma <- 0.1
-df <- 3
+df <- 4
 gp_scale <- 1.2
-n_draws <- 6
-nknots <- 10
+n_draws <- 4
+nknots <- 9
 TOL <- 0.2 # %
 TOL_df <- .25 # %
 
-skip_on_cran()
 test_that("mvt-norm model fits", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
   set.seed(SEED)
 
   s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
@@ -37,13 +40,15 @@ test_that("mvt-norm model fits", {
 
   b <- broom::tidyMCMC(m$model, estimate.method = "median")
   expect_equal(b[b$term == "sigma[1]", "estimate"], sigma, tol = sigma * TOL)
-  # expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
-  # expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
-  # expect_equal(b[b$term == "df[1]", "estimate"], df, tol = df * TOL_df)
+  expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
+  expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
+  expect_equal(b[b$term == "df[1]", "estimate"], df, tol = df * TOL_df)
 })
 
-skip_on_cran()
 test_that("mvt-nb2 model fits", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
   set.seed(SEED)
 
   sigma <- 8
@@ -52,27 +57,29 @@ test_that("mvt-nb2 model fits", {
   n_draws <- 8
   gp_scale <- 1.6
 
-  # s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
-  #   gp_sigma = gp_sigma, sd_obs = sigma, n_knots = nknots,
-  #   obs_error = "nb2", b0 = b0)
+  s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
+    gp_sigma = gp_sigma, sd_obs = sigma, n_knots = nknots,
+    obs_error = "nb2", b0 = b0)
   # print(s$plot)
-  #
-  # m <- rrfield(y ~ 1, data = s$dat, time = "time",
-  #   lat = "lat", lon = "lon", nknots = nknots,
-  #   iter = ITER * 2, chains = CHAINS, obs_error = "nb2",
-  #   estimate_df = FALSE, fixed_df_value = df,
-  #   control = list(adapt_delta = 0.9), seed = SEED)
 
-  # b <- tidy(m, estimate.method = "median")
-  # expect_equal(b[b$term == "nb2_phi[1]", "estimate"], sigma, tol = sigma * TOL)
-  # expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
-  # expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
-  # expect_equal(b[b$term == "B[1]", "estimate"], b0, tol = gp_scale * TOL)
+  m <- rrfield(y ~ 1, data = s$dat, time = "time",
+    lat = "lat", lon = "lon", nknots = nknots,
+    iter = ITER * 2, chains = CHAINS, obs_error = "nb2",
+    estimate_df = FALSE, fixed_df_value = df,
+    control = list(adapt_delta = 0.9), seed = SEED)
+
+  b <- tidy(m, estimate.method = "median")
+  expect_equal(b[b$term == "nb2_phi[1]", "estimate"], sigma, tol = sigma * TOL)
+  expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
+  expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
+  expect_equal(b[b$term == "B[1]", "estimate"], b0, tol = gp_scale * TOL)
 })
 
-
-skip_on_cran()
 test_that("mvt-gamma model fits", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
   set.seed(SEED)
 
   sigma <- 0.3
@@ -81,20 +88,20 @@ test_that("mvt-gamma model fits", {
   n_draws <- 8
   gp_scale <- 1.6
 
-  # s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
-  #   gp_sigma = gp_sigma, sd_obs = sigma, n_knots = nknots,
-  #   obs_error = "gamma", b0 = b0)
-  # # print(s$plot)
-  #
-  # m <- rrfield(y ~ 1, data = s$dat, time = "time",
-  #   lat = "lat", lon = "lon", nknots = nknots,
-  #   iter = ITER * 1.5, chains = CHAINS, obs_error = "gamma",
-  #   estimate_df = FALSE, fixed_df_value = df, seed = SEED)
-  # print(m)
+  s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
+    gp_sigma = gp_sigma, sd_obs = sigma, n_knots = nknots,
+    obs_error = "gamma", b0 = b0)
+  # print(s$plot)
 
-  # b <- tidy(m, estimate.method = "median")
-  # expect_equal(b[b$term == "CV[1]", "estimate"], sigma, tol = sigma * TOL)
-  # expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
-  # expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
-  # expect_equal(b[b$term == "B[1]", "estimate"], b0, tol = gp_scale * TOL)
+  m <- rrfield(y ~ 1, data = s$dat, time = "time",
+    lat = "lat", lon = "lon", nknots = nknots,
+    iter = ITER * 1.5, chains = CHAINS, obs_error = "gamma",
+    estimate_df = FALSE, fixed_df_value = df, seed = SEED)
+  print(m)
+
+  b <- tidy(m, estimate.method = "median")
+  expect_equal(b[b$term == "CV[1]", "estimate"], sigma, tol = sigma * TOL)
+  expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
+  expect_equal(b[b$term == "gp_scale", "estimate"], gp_scale, tol = gp_scale * TOL)
+  expect_equal(b[b$term == "B[1]", "estimate"], b0, tol = gp_scale * TOL)
 })
