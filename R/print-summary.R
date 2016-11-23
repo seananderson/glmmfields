7dc447fb9ad055f156eac218f85f28d61668e3aa
Sean Anderson <sean@seananderson.ca>
2016-11-22 16:20:19 -0800

    Fix warnings on Travis

--- R/print-summary.R
+++ R/print-summary.R
@@ -4,11 +4,17 @@ print.rrfield <- function(x, ...) {
   print(x$model, ...)
 }
 
+#' Tidy model output
+#'
+#' @param x Output from \code{\link{rrfield}}
 #' @export
 tidy <- function(x, ...){
   UseMethod("tidy")
 }
 
+#' Tidy model output
+#'
+#' @param x Output from \code{\link{rrfield}}
 #' @export
 tidy.rrfield <- function(x, ...) {
   broom::tidyMCMC(x$model, ...)
