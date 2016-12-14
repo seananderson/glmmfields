327eef6a02b932a12fa39e107492e2646ce78098
Sean Anderson <sean@seananderson.ca>
2016-12-14 09:24:51 -0800

    More tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -342,3 +342,49 @@ test_that("mvt-norm estimates ar process *with* year random walk effects", {
   expect_equal(b[b$term == "ar[1]", "estimate"], ar, tol = ar * TOL)
   expect_equal(b[b$term == "ar[1]", "estimate"], ar, tol = ar * TOL)
 })
+
+# --------------------------
+# global int + AR RF
+
+test_that("mvt-norm estimates global int + AR RF", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+
+  set.seed(SEED*2)
+
+  gp_sigma <- 0.2
+  sigma <- 0.2
+  df <- 6
+  gp_scale <- 1.8
+  n_draws <- 20
+  nknots <- 10
+  ar <- 0.75
+  B <- vector(mode = "double", length = n_draws)
+  B[1] <- 6
+  year_sigma <- 0.0001
+  for (i in 2:length(B)) {
+    B[i] <- B[i-1] + rnorm(1, 0, year_sigma) # random walk
+  }
+
+  s <- sim_rrfield(df = df, n_draws = n_draws, gp_scale = gp_scale,
+    gp_sigma = gp_sigma, sd_obs = sigma, n_knots = nknots, ar = ar,
+    B = B, X = model.matrix(~ a - 1, data.frame(a = gl(n_draws, 100))))
+  print(s$plot)
+  library(ggplot2); ggplot(s$dat, aes(time, y)) +
+    geom_point(alpha = 0.5, position = position_jitter(width = 0.2))
+
+  m <- rrfield(y ~ 1, data = s$dat, time = "time", station = "station_id",
+    lat = "lat", lon = "lon", nknots = nknots,
+    iter = ITER, chains = CHAINS, seed = SEED,
+    estimate_df = TRUE, year_re = FALSE,
+    control = list(adapt_delta = 0.95),
+    estimate_ar = TRUE, prior_intercept = student_t(99, 0, 30))
+  m
+
+  b <- tidy(m, estimate.method = "median")
+  expect_equal(b[b$term == "sigma[1]", "estimate"], sigma, tol = sigma * TOL)
+  expect_equal(b[b$term == "gp_sigma", "estimate"], gp_sigma, tol = gp_sigma * TOL)
+  expect_equal(b[b$term == "df[1]", "estimate"], df, tol = df * TOL_df)
+  expect_equal(b[b$term == "ar[1]", "estimate"], ar, tol = ar * TOL)
+})
